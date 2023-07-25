aa99eaf8a2978aeb2e6ca878f3a3646892357bbf
Stephen Liptrott <stephen.liptrott@ping.network>
2023-07-25 18:04:22 +0100

    fix: Added login message

--- main.go
+++ main.go
@@ -108,6 +108,7 @@ func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 	freedns.LogInfo(fmt.Sprintf("Login username: \"%s\" password %s\n", username, password))
 	err = dnsObj.Login(username, password)
 	if err != nil {
+		freedns.LogInfo("Login Error");
 		return err
 	}
 
